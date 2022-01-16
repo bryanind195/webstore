e000f87692b67e84877a2d45a7c74a18aca801fb
Angel ind <bryanind195@gmail.com>
2022-01-16 18:02:52 -0500

    se crean los paginas y las rutas para la webstore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/Homepage';
+import {Route, BrowserRouter , Routes} from 'react-router-dom'
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Productinfo from './pages/Productinfo';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+        <Routes>
+
+            <Route path='/' exact element={<Homepage/>} />
+            <Route path='/login' exact element={<LoginPage/>} />
+            <Route path='/register' exact element={<RegisterPage/>} />
+            <Route path='/productinfo' exact element={<Productinfo/>} />
+            <Route path='/cart' exact element={<CartPage/>} />
+
+        </Routes>
+       </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Productinfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Productinfo() {
+    return (
+        <div>
+            <h1>Product Info</h1>
+        </div>
+    )
+}
+
+export default Productinfo
